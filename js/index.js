0d13c4c1e255370697b2096aa8dea53f95a89b94
Riju158 <riju158basak@gmail.com>
2024-07-30 13:40:26 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const menuber=document.getElementById("menuber");
+const navlist=document.getElementById("navlist");
+
+menuber.addEventListener('click', listshow)
+
+function listshow (){
+    navlist.classList.toggle('show')
+
+}
+
+    
